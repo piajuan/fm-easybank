b95b3bd0d984a08ae3b56a53d2187d3e64a671e9
Pia Juan <piajuan.dev@gmail.com>
2023-05-16 15:05:56 +0800

    added fonts and scrollrevealjs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-var $ = require("jquery");
+import ScrollReveal from "scrollreveal";
+const $ = require("jquery");
 
-$(".nav__icon").click(function () {
-  $(".nav__icon").toggleClass("is-clicked");
-  $(".nav__mobile-menu").toggleClass("is-opened");
-  $("body").toggleClass("menu-open");
-});
+function toggleMobileMenu() {
+  $(".nav__icon").click(() => {
+    $(".nav__icon").toggleClass("is-clicked");
+    $(".nav__mobile-menu").toggleClass("is-opened");
+    $("body").toggleClass("menu-open");
+  });
+
+  $("main").click(() => {
+    $(".nav__icon").removeClass("is-clicked");
+    $(".nav__mobile-menu").removeClass("is-opened");
+    $("body").removeClass("menu-open");
+  });
+}
+
+function activateScrollReveal() {
+  window.sr = ScrollReveal();
+  window.sr.reveal(".sr-animate");
+}
+
+toggleMobileMenu();
+activateScrollReveal();
